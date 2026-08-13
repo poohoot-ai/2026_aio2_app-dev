a2d0c1fe2fc618fc58ebded6798d1aca8ad83050
poohoot-ai <poohoot.ai@gmail.com>
2026-08-13 17:31:43 +0900

    FastAPI 2일차 - 요청 데이터와 검증

--- 01-fastapi-basic/main.py
+++ 01-fastapi-basic/main.py
@@ -1,9 +1,36 @@
 from fastapi import FastAPI
+from fastapi import status
+from fastapi import HTTPException
+from pydantic import BaseModel
+from pydantic import Field
+from pydantic import field_validator
 
 app = FastAPI()
 
 from fastapi.staticfiles import StaticFiles
 app.mount("/static", StaticFiles(directory="static"), name="static")
+
+class Publisher(BaseModel):
+    name: str
+    city: str = "고양"
+
+class BookCreate(BaseModel):
+    title: str = Field(min_length=1, max_length=100)
+    author: str = Field(min_length=1, max_length=50)
+    year: int = Field(ge=1900, le=2026)
+    tags: list[str] = Field(default_factory=list)
+    publisher: Publisher | None = None
+
+    # @field_validator("title")
+    # @classmethod
+    def strip_title(cls, v: str) -> str:
+        v = v.strip()
+        if not v:
+            raise ValueError("제목은 필수 입력입니다. (공백안됨)")
+        return v
+
+class BookResponse(BookCreate):
+    id: int
 
 books = [
     {"id": 1, "title": "파이썬 입문", "author": "김철수", "year": 2021},
@@ -26,7 +53,7 @@ def info():
     return {"name": "도서 관리 API", "version": "0.1.0"}
 
 # 도서의 목록을 제공하는 엔드포인트
-@app.get("/books")
+@app.get("/books", response_model=list[BookResponse])
 def list_books():
     return books
 
@@ -36,9 +63,43 @@ def search_books(keyword: str = ""):
         return books
     return [b for b in books if keyword in b["title"]]
 
-@app.get("/books/{book_id}")
+@app.get("/books/filter")
+def filter_books(keyword: str = "", sort: str = ""):
+    result = books
+
+    # for book in books:
+    if keyword:
+        # 리스트 컴프리헨션 - for + if > 리스트
+        result = [b for b in result if b["author"] == keyword]
+
+        if sort == "year":
+            result = sorted(result, key=lambda b: b["year"])
+
+    return result
+
+@app.get("/books/page")
+def page_books(skip: int = 0, limit: int = 2):
+    return books[skip: skip + limit]
+
+@app.get("/books/{book_id}", response_model=BookResponse)
 def read_book(book_id: int):
     for book in books:
         if book["id"] == book_id:
             return book
-    return {"error": "not found"}
+    raise HTTPException(status_code=404, detail="도서를 찾을 수 없습니다")
+
+@app.post("/books", response_model=BookResponse, status_code=status.HTTP_201_CREATED)
+def create_book(book: BookCreate):
+    for b in books:
+        if b["title"] == book.title:
+            raise HTTPException(status_code=409, detail="기존에 등록된 도서입니다")
+
+    new_id = max([b["id"] for b in books], default=0) + 1
+    new_book = {"id": new_id, **book.model_dump()}
+    books.append(new_book)
+    return new_book
+
+# 테스트 시나리오
+# 1. 새로운 책 등록
+# 2. 책 목록을 조회
+# 3. 등록한 책을 검색
